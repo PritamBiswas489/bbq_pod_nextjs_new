98947bf823ba109203477d557b7c94af047a690f
mithunraja <mithun.dey1979@gmail.com>
2026-02-11 18:19:56 +0530

    product

--- src/components/exteriorProductPage/index.jsx
+++ src/components/exteriorProductPage/index.jsx
@@ -1,53 +1,84 @@
 // components/ExteriorColours.js
 
-import { Container, Row, Col } from "react-bootstrap";
-import { FaPalette } from "react-icons/fa";
+import { Container } from "react-bootstrap";
 import styles from "./index.module.scss";
 import Link from "next/link";
 import TitleHeader from "../titleHeader";
-import { TiArrowRightOutline } from "react-icons/ti";
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next";
 import Image from "next/image";
 
- 
+const ExteriorProductPage = ({
+  title,
+  subtitle,
+  imageRef,
+  imageReftwo,
+  items,
+}) => {
+  const { t } = useTranslation("common");
 
-const ExteriorProductPage = ({title, subtitle, imageRef, imageReftwo, items}) => {
-  const { t } = useTranslation('common')
   return (
     <div className={styles.exteriorColours}>
-      
-      <TitleHeader
-        whyChoose={[]}
-        title={title}
-        subtitle={subtitle}
-      />
+      <TitleHeader whyChoose={[]} title={title} subtitle={subtitle} />
+
       <Container className={styles.container}>
-        {imageRef && <Image src={imageRef} alt="Product Image" width={800} height={600} />}
-        {imageReftwo && <Image src={imageReftwo} alt="Product Image" width={800} height={600} />}
-        <div className={styles.colorGrid}>
-          {items.map((item, index) => (
-            <div
-              key={index}
-              className={styles.swatchWrapper}
-              data-aos="zoom-in-up"
-              data-aos-duration="1000"
-              data-aos-once="true"
-              
-            >
-              {/* <Link href={`/${color.slug}`} className={styles.swatchLink}> */}
-              <Link href={`#`} className={styles.swatchLink}>
-                {/* <div
-                  className={styles.swatch}
-                  style={{ backgroundColor: '#ccc' }}
-                /> */}
-                {item.image && <Image src={item.image} alt={`${item.colorName} - ${item.modelName}`} width={300} height={200} style={{borderRadius: '100%'}} />}
-                <h6>{item.colorName}</h6>
-                <p className={styles.label}>{item.modelName}</p>
-              </Link>
-            </div>
-          ))}
+        <div className="d-flex flex-wrap align-items-center">
+          {/* ✅ IMAGE WRAPPER */}
+          <div className={styles.imgWrap}>
+            {/* 🔹 First Image */}
+            {imageRef && (
+              <div className={styles.imageBox}>
+                <Image
+                  src={imageRef}
+                  alt="Product Image One"
+                  width={800}
+                  height={600}
+                />
+              </div>
+            )}
+
+            {/* 🔹 Second Image */}
+            {imageReftwo && (
+              <div className={styles.imageBox}>
+                <Image
+                  src={imageReftwo}
+                  alt="Product Image Two"
+                  width={800}
+                  height={600}
+                />
+              </div>
+            )}
+          </div>
+
+          {/* ✅ COLOR GRID */}
+          <div className={styles.colorGrid}>
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className={styles.swatchWrapper}
+                data-aos="zoom-in-up"
+                data-aos-duration="1000"
+                data-aos-once="true"
+              >
+                <Link href="#" className={styles.swatchLink}>
+                  {item.image && (
+                    <Image
+                      src={item.image}
+                      alt={item.colorName}
+                      width={300}
+                      height={300}
+                      style={{ borderRadius: "20px" }}
+                    />
+                  )}
+
+                  <h6>{item.colorName}</h6>
+                  {item.modelName && (
+                    <p className={styles.label}>{item.modelName}</p>
+                  )}
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
-        
       </Container>
     </div>
   );
--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -25,6 +25,37 @@ import REF002 from "@/assets/front/images/RefImage/REF002.png";
 import REF003 from "@/assets/front/images/RefImage/REF003.png";
 import REF004 from "@/assets/front/images/RefImage/REF004.png";
 
+import bg1 from "@/assets/front/images/image-shade/athens-white.webp";
+import bg2 from "@/assets/front/images/image-shade/medium-gray.webp";
+import bg3 from "@/assets/front/images/image-shade/purple-gray.webp";
+import bg4 from "@/assets/front/images/image-shade/black-sand.webp";
+
+import bg5 from "@/assets/front/images/image-shade/beige-matte.webp";
+import bg6 from "@/assets/front/images/image-shade/starry-silver.webp";
+import bg7 from "@/assets/front/images/image-shade/khaki-gray.webp";
+import bg8 from "@/assets/front/images/image-shade/morandi-gray.webp";
+import bg9 from "@/assets/front/images/image-shade/light-gray.webp";
+import bg10 from "@/assets/front/images/image-shade/silver-gray.webp";
+import bg11 from "@/assets/front/images/image-shade/traffic-gray.webp";
+import bg12 from "@/assets/front/images/image-shade/gray-flash.webp";
+import bg13 from "@/assets/front/images/image-shade/light-green.webp";
+import bg14 from "@/assets/front/images/image-shade/retro-green.webp";
+import bg15 from "@/assets/front/images/image-shade/olive-green.webp";
+import bg16 from "@/assets/front/images/image-shade/lake-green.webp";
+
+import bg17 from "@/assets/front/images/image-shade/camel-gray.webp";
+import bg18 from "@/assets/front/images/image-shade/khaki-wood.webp";
+import bg19 from "@/assets/front/images/image-shade/black-walnut.webp";
+import bg20 from "@/assets/front/images/image-shade/cherry-blossom.webp";
+
+import bg21 from "@/assets/front/images/image-shade/tree-bark.webp";
+import bg22 from "@/assets/front/images/image-shade/rhino-skin.webp";
+
+import bg23 from "@/assets/front/images/image-shade/calacatta-white.webp";
+import bg24 from "@/assets/front/images/image-shade/cave-stone-beige.webp";
+import bg25 from "@/assets/front/images/image-shade/bosnia-gray.webp";
+import bg26 from "@/assets/front/images/image-shade/spanish-black-and-whitw-root.webp";
+
 export default function Products() {
   const { t } = useTranslation("common");
   const router = useRouter();
@@ -99,22 +130,22 @@ export default function Products() {
             {
               colorName: "ATHENS WHITE",
               modelName: "SP00321",
-              image: null,
+              image: bg1,
             },
             {
               colorName: "MEDIUM GRAY",
               modelName: "SP00071",
-              image: null,
+              image: bg2,
             },
             {
               colorName: "PURPLE GRAY",
               modelName: "CP63203SF",
-              image: null,
+              image: bg3,
             },
             {
               colorName: "BLACK SAND",
               modelName: "SP00368",
-              image: null,
+              image: bg4,
             },
           ]}
         />
@@ -127,62 +158,62 @@ export default function Products() {
             {
               colorName: "BEIGE MATTE",
               modelName: "CY0011",
-              image: null,
+              image: bg5,
             },
             {
               colorName: "STARRY SILVER",
               modelName: "CPJ103",
-              image: null,
+              image: bg6,
             },
             {
               colorName: "KHAKI GRAY",
               modelName: "FTE1014898T",
-              image: null,
+              image: bg7,
             },
             {
               colorName: "MORANDI GRAY",
               modelName: "CY0037",
-              image: null,
+              image: bg8,
             },
             {
               colorName: "LIGHT GRAY",
               modelName: "SP00598",
-              image: null,
+              image: bg9,
             },
             {
               colorName: "SILVER GRAY",
               modelName: "CP61773SF",
-              image: null,
+              image: bg10,
             },
             {
               colorName: "TRAFFIC GRAY",
               modelName: "FTE101554",
-              image: null,
+              image: bg11,
             },
             {
               colorName: "GRAY FLASH",
               modelName: "SP00111",
-              image: null,
+              image: bg12,
             },
             {
               colorName: "LIGHT GREEN",
               modelName: "CY35109",
-              image: null,
+              image: bg13,
             },
             {
               colorName: "RETRO GREEN",
               modelName: "CY0047",
-              image: null,
+              image: bg14,
             },
             {
               colorName: "OLIVE GREEN",
               modelName: "CP71732C",
-              image: null,
+              image: bg15,
             },
             {
               colorName: "LAKE GREEN",
               modelName: "JXBD0089",
-              image: null,
+              image: bg16,
             },
           ]}
         />
@@ -195,22 +226,22 @@ export default function Products() {
             {
               colorName: "CAMEL GRAY",
               modelName: "CP63195S + JC-8193Y-2",
-              image: null,
+              image: bg17,
             },
             {
               colorName: "KHAKI WOOD",
               modelName: "XGHY-P2412 + XCH-PS9015",
-              image: null,
+              image: bg18,
             },
             {
               colorName: "BLACK WALNUT",
               modelName: "CP24938SF + JT4K9015-3",
-              image: null,
+              image: bg19,
             },
             {
               colorName: "CHERRY BLOSSOM",
               modelName: "M243556P + JR-H1823",
-              image: null,
+              image: bg20,
             },
           ]}
         />
@@ -223,12 +254,12 @@ export default function Products() {
             {
               colorName: "TREE BARK",
               modelName: null,
-              image: require('@/assets/front/images/ss-tree-bark.jpg'),
+              image: bg21,
             },
             {
               colorName: "RHINO SKIN",
               modelName: null,
-              image: require('@/assets/front/images/ss-rhino-skin.jpg'),
+              image: bg22,
             },
           ]}
         />
@@ -241,22 +272,22 @@ export default function Products() {
             {
               colorName: "CALACATTA WHITE",
               modelName: null,
-              image: null,
+              image: bg23,
             },
             {
               colorName: "CAVE STONE BEIGE",
               modelName: null,
-              image: null,
+              image: bg24,
             },
             {
               colorName: "BOSNIA GREY",
               modelName: null,
-              image: null,
+              image: bg25,
             },
             {
               colorName: "SPANISH BLACK AND WHITE ROOT",
               modelName: null,
-              image: null,
+              image: bg26,
             },
           ]}
         />
